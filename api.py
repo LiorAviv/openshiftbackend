f648c6807ad1ff6207d0205e5644bdedd53ff42a
Lior <lioraviv1312@gmail.com>
2023-05-17 15:04:42 +0300

    lol

--- api.py
+++ api.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently .</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """
+    return render_template('index.html')
 
 @app.route("/getlocations")
 def getlocations():
